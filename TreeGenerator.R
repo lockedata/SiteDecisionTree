1b4c10a504cefca5774c66faa20169e43d22b6f3
Steph Locke <steph.locke@censornet.com>
2018-01-03 11:22:46 +0000

    Tweaks

--- TreeGenerator.R
+++ TreeGenerator.R
@@ -1,12 +1,17 @@
 library(collapsibleTree)
 `Locke Data`<-readr::read_csv("SiteDecisionTree.csv")
+`Locke Data`$end<-glue::glue('<a href="{url}">{text}</a>',url=`Locke Data`$URL,text=`Locke Data`$L3)
+
 tree<-collapsibleTree(
   `Locke Data`,
   c("L1","L2","L3"),
   collapsed = FALSE,
-  nodeSize = "leafCount"
+  nodeSize = "leafCount",
+  tooltip = TRUE,
+  tooltipHtml = "end"
 )
 
 tree
 
 htmlwidgets::saveWidget(tree, "tree.html")
+htmlwidgets::saveWidget(tree, "tree_full.html", selfcontained = FALSE)
